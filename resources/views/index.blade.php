0125789d8966f48901c70cd96e70656bc8807d45
imranshuvo <imrankhanshuvo@gmail.com>
2023-08-31 03:33:32 +0600

    Init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+   <div>Just checking in from inside the index.blade.php </div>
+</x-app-layout>
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+const { colors } = require('tailwindcss/defaultTheme');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                black: colors.black,
+                white: colors.white,
+                gray: colors.gray,
+                'gray-background':'#f7f8fc',
+            },
+            fontFamily: {
+                sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
